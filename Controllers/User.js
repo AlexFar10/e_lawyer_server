6a9996406b707ef689eb9a7f72429da49dd6f3f7
Farcas Alexandru <alexandru.farcas01@e-uvt.ro>
2023-03-22 19:34:49 +0200

    Add user role and validation

--- Controllers/User.js
+++ Controllers/User.js
@@ -1,117 +1,85 @@
-const bcrypt = require('bcrypt')
-const User = require('../Models/User');
-const jwt = require("jsonwebtoken")
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 const env = {
-    JWT_KEY: "super_secret"
+    JWT_KEY: 'super_secret'
 };
 
 module.exports = {
-    getAllUsers: async (req, res, next) => {
+    getAllUsers: async (req, res) => {
         try {
-            const result = await User.find({}, { __v: 0, _id: 0 })
-            res.send(result)
+            const result = await User.find({}, { __v: 0, _id: 0 });
+            res.send(result);
         } catch (error) {
-            console.log(error.message)
+            console.error(error.message);
+            res.status(500).json({ error: 'Server error' });
         }
     },
 
-    createNewUser: async (req, res, next) => {
+    createNewUser: async (req, res) => {
         try {
             const existingUser = await User.findOne({ Email: req.body.Email });
+
             if (existingUser) {
-                return res.status(409).json({
-                    message: "Email address is already registered",
-                });
+                return res.status(400).send('Email already exists');
             }
 
-            let role = "client";
-            if (req.body.Email.endsWith("@elawyer_lawyer.com")) {
-                role = "lawyer";
-            } else if (req.body.Email.endsWith("@elawyer_admin.com")) {
-                role = "admin";
-            }
+            const role = req.body.Email.endsWith('@elawyer_lawyer.com')
+                ? 'lawyer'
+                : req.body.Email.endsWith('@elawyer_admin.com')
+                    ? 'admin'
+                    : 'client';
 
             const hashedPassword = await bcrypt.hash(req.body.Password, 10);
-
             const newUser = new User({
                 Name: req.body.Name,
                 Surname: req.body.Surname,
                 Email: req.body.Email,
                 Password: hashedPassword,
-                role: role,
+                role:role,
             });
-
             await newUser.save();
-
-            return res.status(201).json({
-                message: "User created",
-            });
+            return res.status(201).json({ message: 'User created' });
         } catch (error) {
-            console.log(error);
-            return res.status(500).json({
-                error: error.message,
-            });
+            console.error(error.message);
+            res.status(500).json({ error: 'Server error' });
+
         }
     },
 
-    deleteUser: async (req, res, next) => {
+    deleteUser: async (req, res) => {
         try {
             await User.deleteOne({ _id: req.params.id });
-            return res.status(200).json({
-                message: "User deleted",
-            });
+            res.status(200).json({ message: 'User deleted' });
         } catch (error) {
-            console.log(error);
-            return res.status(500).json({
-                error: error.message,
-            });
+            console.error(error.message);
+            res.status(500).json({ error: 'Server error' });
         }
     },
 
-    loginUser: async (req, res, next) => {
+    loginUser: async (req, res) => {
         try {
             const user = await User.findOne({ Email: req.body.Email });
             if (!user) {
-                return res.status(401).json({
-                    message: "Auth failed"
-                });
+                return res.status(401).json({ message: 'Invalid email or password' });
             }
+
             const isMatch = await bcrypt.compare(req.body.Password, user.Password);
             if (!isMatch) {
-                return res.status(401).json({
-                    message: "Auth failed"
-                });
+                return res.status(401).json({ message: 'Invalid email or password' });
             }
 
-            let role;
-            if (user.Email.endsWith("@elawyer_lawyer.com")) {
-                role = "lawyer";
-            } else if (user.Email.endsWith("@elawyer_admin.com")) {
-                role = "admin";
-            } else {
-                role = "client";
-            }
+            const role = req.body.Email.endsWith('@elawyer_lawyer.com')
+                ? 'lawyer'
+                : req.body.Email.endsWith('@elawyer_admin.com')
+                    ? 'admin'
+                    : 'client';
 
-            const token = jwt.sign(
-                {
-                    userId: user._id,
-                    role: role
-                },
-                env.JWT_KEY,
-                {
-                    expiresIn: "1h"
-                }
-            );
-            res.status(200).json({
-                message: "Auth successful",
-                token: token
-            });
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
+            const token = jwt.sign({ userId: user._id, role }, env.JWT_KEY, { expiresIn: '1h' });
+            res.status(200).json({ message: 'Login successful', token });
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({ error: 'Server error' });
         }
-    }
+    },
 };
--- Models/User.js
+++ Models/User.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const Joi = require('joi');
 
-const UserSchema = new Schema({
-
+const userSchema = new mongoose.Schema({
     Name: {
         type: String,
         required: true
@@ -16,34 +14,34 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        validate: {
-            validator: (email) => {
-                const schema = Joi.string().email();
-                return schema.validate(email).error === null;
-            },
-            message: 'Invalid email format',
-        },
     },
     Password: {
         type: String,
         required: true,
         validate: {
-            validator: (password) => {
-                const schema = Joi.string()
-                    .min(8)
-                    .max(32)
-                    .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-={}|\\[\\]\\\\;\':"<>?,./])'))
-                    .messages({
-                        'string.min': 'Password must be at least 8 characters',
-                        'string.max': 'Password can be at most 32 characters',
-                        'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one symbol',
-                    });
-                return schema.validate(password).error === null;
+            validator: function(v) {
+                const passwordSchema = Joi.object({
+                    password: Joi.string()
+                        .min(8)
+                        .max(32)
+                        //.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=[\]{}|\\;:'",.<>/?])/)
+                        .required(),
+                });
+
+             //   const { error } = passwordSchema.validate({ password: v });
+              //  return error === undefined;
             },
-            message: 'Invalid password format',
+            //message: props =>
+            //    `${props.value} is not a valid password! Password must be at least 8 characters, at most 32 characters, contain at least one uppercase letter, one lowercase letter, one number, and one symbol`
         },
+    },
+    role: {
+        type: String,
+        enum: ['client', 'lawyer', 'admin'],
+        default: 'client'
     }
 });
 
-const User = mongoose.model('user', UserSchema);
+const User = mongoose.model('User', userSchema);
+
 module.exports = User;
